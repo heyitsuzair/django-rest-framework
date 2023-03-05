d6006a198c0be8f41accf6b5da5b7ffd711b24a2
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-03-05 05:39:08 +0500

    Concrete View

--- api/views.py
+++ api/views.py
@@ -4,7 +4,7 @@ from utils.HttpErrors import HttpNotFound, HttpOK, HttpBadRequest
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import GenericAPIView, ListCreateAPIView,RetrieveUpdateDestroyAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 # Create your views here.
 
@@ -66,27 +66,36 @@ from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveMode
 #         return HttpNotFound('Student Not Found!')
 
 # List And Create - Primary Key (pk) Param Not Required
-class StudentLCAPI(GenericAPIView, ListModelMixin, CreateModelMixin):
+# class StudentLCAPI(GenericAPIView, ListModelMixin, CreateModelMixin):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+
+#     def get(self, req, *args, **kwargs):
+#         return self.list(req, *args, **kwargs)
+
+#     def post(self, req, *args, **kwargs):
+#         return self.create(req, *args, **kwargs)
+
+
+# # Retreive Update And Delete - Primary Key (pk) Required
+# class StudentGPD(GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+
+#     def get(self, req, *args, **kwargs):
+#         return self.retrieve(req, *args, **kwargs)
+
+#     def put(self, req, *args, **kwargs):
+#         return self.update(req, *args, **kwargs)
+
+#     def delete(self, req, *args, **kwargs):
+#         return self.destroy(req, *args, **kwargs)
+
+class StudentLCAPI(ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
-    def get(self, req, *args, **kwargs):
-        return self.list(req, *args, **kwargs)
 
-    def post(self, req, *args, **kwargs):
-        return self.create(req, *args, **kwargs)
-
-
-# Retreive Update And Delete - Primary Key (pk) Required
-class StudentGPD(GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
+class StudentGPD(RetrieveUpdateDestroyAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-
-    def get(self, req, *args, **kwargs):
-        return self.retrieve(req, *args, **kwargs)
-
-    def put(self, req, *args, **kwargs):
-        return self.update(req, *args, **kwargs)
-
-    def delete(self, req, *args, **kwargs):
-        return self.destroy(req, *args, **kwargs)
