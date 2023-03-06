fbcc5baa24b6e6a11c1a6d84a405b5ef5d05631d
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-03-06 05:28:42 +0500

    JWT

--- restframework/urls.py
+++ restframework/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from api import views
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 # Creating Router Object
 router = DefaultRouter()
@@ -11,7 +12,10 @@ router.register('stuinfo', views.StudentModelViewSet, basename='student')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls))
+    path('', include(router.urls)),
     # path('stuinfo/', views.StudentLCAPI.as_view()),
     # path('stuinfo/<int:pk>', views.StudentGPD.as_view()),
+    path('gettoken/', TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path('refreshtoken/', TokenRefreshView.as_view(), name="token_refresh"),
+    path('verifytoken/', TokenVerifyView.as_view(), name="token_refresh")
 ]
